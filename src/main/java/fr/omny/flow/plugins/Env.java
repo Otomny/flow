efa9062acf18a61836a666f78b300f64322a58cf
FabienCAYRE <fabiencayre81@gmail.com>
2023-02-13 14:27:02 +0100

    Environment variable helper class

---
+++ src/main/java/fr/omny/flow/plugins/Env.java
@@ -0,0 +1,42 @@
+package fr.omny.flow.plugins;
+
+import java.util.Optional;
+
+/**
+ * Environment helper class
+ */
+public final class Env {
+	
+	public static final String SERVER_NAME = "FLOW_SERVER_NAME";
+	public static final String DATABASE_NAME = "FLOW_DATABASE_NAME";
+
+	/**
+	 * 
+	 * @return Server name
+	 */
+	public static String getServerName(){
+		return Env.get(Env.SERVER_NAME, "server");
+	}
+
+	/**
+	 * Get environment variable
+	 * @param key
+	 * @return
+	 */
+	public static Optional<String> get(String key){
+		return Optional.ofNullable(System.getenv(key));
+	}
+
+	/**
+	 * Get environment variable
+	 * @param key
+	 * @param fallBack
+	 * @return
+	 */
+	public static String get(String key, String fallBack){
+		return Optional.ofNullable(System.getenv(key)).orElse(fallBack);
+	}
+
+	private Env(){}
+
+}
