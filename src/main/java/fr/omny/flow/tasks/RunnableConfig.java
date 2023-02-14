4f6d0465340c233dd88c6198dad047f560a46a79
FabienCAYRE <fabiencayre81@gmail.com>
2023-02-14 13:55:35 +0100

    Fix retention &  target

--- src/main/java/fr/omny/flow/tasks/RunnableConfig.java
+++ src/main/java/fr/omny/flow/tasks/RunnableConfig.java
@@ -1,30 +1,42 @@
 package fr.omny.flow.tasks;
 
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Configure the runnable to be executed as a component
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface RunnableConfig {
 
 	/**
 	 * The name of the runnable in case of logging
+	 * 
 	 * @return
 	 */
 	String value() default "default";
 
 	/**
 	 * Delay in tick until the first call must be made
+	 * 
 	 * @return Amount of ticks before the first run
 	 */
 	long delay() default 0L;
 
 	/**
 	 * Period between two call
+	 * 
 	 * @return Amount of ticks between two run
 	 */
 	long period() default 20L;
 
 	/**
 	 * If it should run in asynchronous thread
+	 * 
 	 * @return True if async,false otherwise
 	 */
 	boolean async() default false;
