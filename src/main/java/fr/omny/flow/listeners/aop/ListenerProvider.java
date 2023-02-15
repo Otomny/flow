436067a86cebd03a7d30720bdd24c897f34d1e8f
FabienCAYRE <fabiencayre81@gmail.com>
2023-02-15 10:50:52 +0100

    Javadoc on how to implement ListenerProvider

--- src/main/java/fr/omny/flow/listeners/aop/ListenerProvider.java
+++ src/main/java/fr/omny/flow/listeners/aop/ListenerProvider.java
@@ -1,5 +1,9 @@
 package fr.omny.flow.listeners.aop;
 
+/**
+ * Implement that class if you want to register listeners on the fly. It scan the implementing class to find each PUBLIC
+ * method that returns {@link org.bukkit.event.Listener} and call them
+ */
 public interface ListenerProvider {
-	
+
 }
